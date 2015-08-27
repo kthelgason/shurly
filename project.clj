8b9835fd4f927bdc7149a0b00ee94eb8fc2cc228
Kári Tristan Helgason <kthelgason@gmail.com>
2015-08-27 21:00:27 +0000

    Remove useless stuff and set ring handler

--- project.clj
+++ project.clj
@@ -1,17 +1,14 @@
 (defproject shurly "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A URL shortener"
   :url "http://shurly.herokuapp.com"
-  :license {:name "FIXME: choose"
-            :url "http://example.com/FIXME"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [ring/ring-devel "1.2.2"]
-                 [ring-basic-authentication "1.0.5"]
-                 [environ "0.5.0"]
-                 [com.cemerick/drawbridge "0.0.6"]]
+                 [environ "0.5.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "shurly-standalone.jar"
+  :ring {:handler shurly.web/app}
   :profiles {:production {:env {:production true}}})
