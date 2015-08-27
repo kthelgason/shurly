5f445d9e9e97eecefa475efc50782f3f18650309
Kári Tristan Helgason <kthelgason@gmail.com>
2015-08-27 21:28:53 +0000

    Adds non-persistent data store for testing

---
+++ src/shurly/data.clj
@@ -0,0 +1,15 @@
+(ns shurly.data)
+
+(def ^:private urls (atom {}))
+
+(defn- get-target [slug]
+  (get-in @urls [(keyword slug) :target]))
+
+(defn store-slug! [slug target]
+  (if-not (get-target slug)
+    (swap! urls #(assoc %1 (keyword slug) {:target target :redirects 0}))))
+
+(defn request-redirect! [slug]
+  (when-let [target (get-target slug)]
+    (swap! urls #(update-in %1 [(keyword slug) :redirects] (fnil inc 0)))
+    target))
