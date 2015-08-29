de13a78554d5614cf9efe8dd9af3829a2798148a
Kári Tristan Helgason <kthelgason@gmail.com>
2015-08-29 17:11:29 +0000

    Moving data persistance layer to datomic. Nothing works yet

--- src/shurly/data.clj
+++ src/shurly/data.clj
@@ -1,15 +1,42 @@
-(ns shurly.data)
+(ns shurly.data
+  (:require [datomic.api :as d]
+            [environ.core :refer [env]]))
 
-(def ^:private urls (atom {}))
+(def uri (env :datomic-url))
 
-(defn- get-target [slug]
-  (get-in @urls [(keyword slug) :target]))
+(def schemas [[{:db/id #db/id[:db.part/db]
+                :db/ident :rule/url
+                :db/valueType :db.type/string
+                :db/cardinality :db.cardinality/one
+                :db/doc "The destination URL of a redirect rule"
+                :db.install/_attribute :db.part/db}]
+              [{:db/id #db/id[:db.part/db]
+                :db/ident :rule/slug
+                :db/valueType :db.type/string
+                :db/cardinality :db.cardinality/one
+                :db/doc "The slug for the redirect rule"
+                :db.install/_attribute :db.part/db}]])
 
-(defn store-slug! [slug target]
-  (if-not (get-target slug)
-    (swap! urls #(assoc %1 (keyword slug) {:target target :redirects 0}))))
+;; Create database and define schema
+(defn create []
+  (d/create-database uri)
+  (let [conn (d/connect uri)]
+    (map #(d/transact conn %1) schemas)))
 
-(defn request-redirect! [slug]
-  (when-let [target (get-target slug)]
-    (swap! urls #(update-in %1 [(keyword slug) :redirects] (fnil inc 0)))
-    target))
+(defn delete []
+  (d/delete-database uri))
+
+(defn recreate []
+  (delete)
+  (create))
+
+(defn conn []
+  (d/connect uri))
+
+(defn db []
+  (d/db (conn)))
+
+;; Wrapper for the query interface
+(def q #(d/q % (db)))
+(def t #(d/transact (conn) %))
+
